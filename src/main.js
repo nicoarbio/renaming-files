3b6b333dd8df39eefe1814785a21d26c82411873
Nicolás Arbio <nicolasarbio@outlook.com>
2022-06-15 17:51:38 -0300

    Primer version funcional con fs

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import fs from 'fs';
+/**
+ * Input:
+ * - Carpetas de archivos a procesar
+ * - Archivo con nombres correctos
+ * 
+ * 
+ */
+
+const FILEPATH = []
+const FILE = ""
+
+fs.renameSync("./src/test_file", "./src/testfile") 
+
+//cantidad de lineas en file == cantidad de archivos en filepath
+//loopear dentro de filepath y hacer un mv 
+
